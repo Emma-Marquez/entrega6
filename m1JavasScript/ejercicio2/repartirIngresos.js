36c9424d2c116684c7e5ff4232af65b5539acef8
Emma-Marquez <marquezluthier@gmail.com>
2023-08-28 19:31:50 -0300

    completo el m1 de javascript desde cero

---
+++ m1JavasScript/ejercicio2/repartirIngresos.js
@@ -0,0 +1,10 @@
+let ingresos = window.prompt("Coloque sus ingresos")
+let necesarios = ingresos*.5
+let opcionales = ingresos*.3
+let ahorro = ingresos*.2
+alert("Usted debe asignar $"+necesarios+" para los gastos necesario, $"+opcionales+" para gastos opcionales y $"+ahorro+" para ahorro e inversión.")
+/* Un modelo común es el modelo
+50-30-20, que consiste en repartir los ingresos
+de esta forma: 50% para los gastos necesarios,
+30% para gastos opcionales y un 20% para
+ahorro e inversión. */
